7d7a8dc62da6c1a626f6e49cc7f19d7602ec4676
Steve Me <steven@congnitoz.com>
2024-06-09 13:10:54 +0800

    1717938655

--- mod02-ai-vision/Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
+++ mod02-ai-vision/Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
@@ -78,21 +78,97 @@ def AnalyzeImage(image_filename, image_data, cv_client):
             print(" Caption: '{}' (confidence: {:.2f}%)".format(caption.text, caption.confidence * 100))
 
     # Get image tags
-
+    if result.tags is not None:
+        print("\nTags:")
+        for tag in result.tags.list:
+            print(" Tag: '{}' (confidence: {:.2f}%)".format(tag.name, tag.confidence * 100))
 
     # Get objects in the image
+    # Get objects in the image
+    if result.objects is not None:
+        print("\nObjects in image:")
 
+        # Prepare image for drawing
+        image = Image.open(image_filename)
+        fig = plt.figure(figsize=(image.width/100, image.height/100))
+        plt.axis('off')
+        draw = ImageDraw.Draw(image)
+        color = 'cyan'
+
+        for detected_object in result.objects.list:
+            # Print object name
+            print(" {} (confidence: {:.2f}%)".format(detected_object.tags[0].name, detected_object.tags[0].confidence * 100))
+            
+            # Draw object bounding box
+            r = detected_object.bounding_box
+            bounding_box = ((r.x, r.y), (r.x + r.width, r.y + r.height)) 
+            draw.rectangle(bounding_box, outline=color, width=3)
+            plt.annotate(detected_object.tags[0].name,(r.x, r.y), backgroundcolor=color)
+
+        # Save annotated image
+        plt.imshow(image)
+        plt.tight_layout(pad=0)
+        outputfile = 'objects.jpg'
+        fig.savefig(outputfile)
+        print('  Results saved in', outputfile)
 
     # Get people in the image
+    # Get people in the image
+    if result.people is not None:
+        print("\nPeople in image:")
 
+        # Prepare image for drawing
+        image = Image.open(image_filename)
+        fig = plt.figure(figsize=(image.width/100, image.height/100))
+        plt.axis('off')
+        draw = ImageDraw.Draw(image)
+        color = 'cyan'
+
+        for detected_people in result.people.list:
+            # Draw object bounding box
+            r = detected_people.bounding_box
+            bounding_box = ((r.x, r.y), (r.x + r.width, r.y + r.height))
+            draw.rectangle(bounding_box, outline=color, width=3)
+
+            # Return the confidence of the person detected
+            #print(" {} (confidence: {:.2f}%)".format(detected_people.bounding_box, detected_people.confidence * 100))
+            
+        # Save annotated image
+        plt.imshow(image)
+        plt.tight_layout(pad=0)
+        outputfile = 'people.jpg'
+        fig.savefig(outputfile)
+        print('  Results saved in', outputfile)
     
 
 def BackgroundForeground(endpoint, key, image_file):
     # Define the API version and mode
     api_version = "2023-02-01-preview"
     mode="backgroundRemoval" # Can be "foregroundMatting" or "backgroundRemoval"
-    
+    #mode="foregroundMatting"
     # Remove the background from the image or generate a foreground matte
+    # Remove the background from the image or generate a foreground matte
+    print('\nRemoving background from image...')
+        
+    url = "{}computervision/imageanalysis:segment?api-version={}&mode={}".format(endpoint, api_version, mode)
+
+    headers= {
+        "Ocp-Apim-Subscription-Key": key, 
+        "Content-Type": "application/json" 
+    }
+
+    image_url="https://github.com/MicrosoftLearning/mslearn-ai-vision/blob/main/Labfiles/01-analyze-images/Python/image-analysis/{}?raw=true".format(image_file)  
+
+    body = {
+        "url": image_url,
+    }
+        
+    response = requests.post(url, headers=headers, json=body)
+
+    image=response.content
+    with open("backgroundForeground.png", "wb") as file:
+        file.write(image)
+    print('  Results saved in backgroundForeground.png \n')
     
 
 
