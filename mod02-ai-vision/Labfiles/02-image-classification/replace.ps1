d572cbd292d97610acf51a59ebdb0d56060de66e
Steve Me <steven@congnitoz.com>
2024-06-09 21:24:18 +0800

    1717968258

--- mod02-ai-vision/Labfiles/02-image-classification/replace.ps1
+++ mod02-ai-vision/Labfiles/02-image-classification/replace.ps1
@@ -1,2 +1,2 @@
-$storageAcct = '<storageAccount>'
+$storageAcct = 'sademoforai7882'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
