ee3c35e8768fbedb00819f1c2c96304571174823
Steve Me <steven@congnitoz.com>
2024-06-12 00:43:33 +0800

    1718153013

--- mod03-ai-language/Labfiles/08-speech-translation/Python/translator/translator.py
+++ mod03-ai-language/Labfiles/08-speech-translation/Python/translator/translator.py
@@ -3,7 +3,8 @@ from datetime import datetime
 import os
 
 # Import namespaces
-
+# Import namespaces
+import azure.cognitiveservices.speech as speech_sdk
 
 def main():
     try:
@@ -16,15 +17,22 @@ def main():
         ai_region = os.getenv('SPEECH_REGION')
 
         # Configure translation
-
+        # Configure translation
+        translation_config = speech_sdk.translation.SpeechTranslationConfig(ai_key, ai_region)
+        translation_config.speech_recognition_language = 'en-US'
+        translation_config.add_target_language('fr')
+        translation_config.add_target_language('es')
+        translation_config.add_target_language('ms')
+        print('Ready to translate from',translation_config.speech_recognition_language)
 
         # Configure speech
-
+        # Configure speech
+        speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
 
         # Get user input
         targetLanguage = ''
         while targetLanguage != 'quit':
-            targetLanguage = input('\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n').lower()
+            targetLanguage = input('\nEnter a target language\n fr = French\n es = Spanish\n ms = Malay\n Enter anything else to stop\n').lower()
             if targetLanguage in translation_config.target_languages:
                 Translate(targetLanguage)
             else:
@@ -38,10 +46,28 @@ def Translate(targetLanguage):
     translation = ''
 
     # Translate speech
+    # Translate speech
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    translator = speech_sdk.translation.TranslationRecognizer(translation_config, audio_config = audio_config)
+    print("Speak now...")
+    result = translator.recognize_once_async().get()
+    print('Translating "{}"'.format(result.text))
+    translation = result.translations[targetLanguage]
+    print(translation)
 
 
     # Synthesize translation
-
+    # Synthesize translation
+    voices = {
+            "fr": "fr-FR-HenriNeural",
+            "es": "es-ES-ElviraNeural",
+            "ms": "ms-MY-YasminNeural"
+    }
+    speech_config.speech_synthesis_voice_name = voices.get(targetLanguage)
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+    speak = speech_synthesizer.speak_text_async(translation).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
 
 
 if __name__ == "__main__":
